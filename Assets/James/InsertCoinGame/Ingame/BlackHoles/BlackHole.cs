3794f3cb42af9bf81f2b3e212f3b14e8a2c68762
Antonio Harger Rivero <antonio.harger@gmail.com>
2019-04-27 23:19:35 -0300

    Gravity WIP

---
+++ Assets/James/InsertCoinGame/Ingame/BlackHoles/BlackHole.cs
@@ -0,0 +1,81 @@
+﻿using James.InsertCoinGame.Ingame.Coins;
+using NaughtyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TonhoHR.ObjectCheckers;
+using UnityEngine;
+
+namespace James.InsertCoinGame.Ingame.BlackHoles
+{
+    public class BlackHole : MonoBehaviour
+    {
+        [SerializeField]
+        private float force = 5;
+        [SerializeField]
+        private float radius = 3;
+        [SerializeField]
+        private TriggerNotifier notifier;
+        [SerializeField]
+        private SphereCollider collider;
+        [SerializeField]
+        private ParticleSystem[] particleSystems;
+        private CheckForObjects<Coin> coinsDetector;
+        #region DebugStuff
+        [ShowNativeProperty]
+        public int DebugCoins { get { return coinsDetector == null ? 0 : coinsDetector.CurrentObjects.Count(); } }
+        #endregion
+
+        private void Start()
+        {
+            coinsDetector = new CheckForObjects<Coin>(notifier);
+            coinsDetector.ObjectEntered += OnCoinEntered;
+            coinsDetector.ObjectLeft += OnCoinLeft;
+
+            ApplyRange();
+        }
+
+        private void OnCoinLeft(Coin coin)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnCoinEntered(Coin coin)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnValidate()
+        {
+            ApplyRange();
+        }
+
+        private void ApplyRange()
+        {
+            foreach (var part in particleSystems)
+            {
+                var emission = part.shape;
+                emission.radius = radius;
+            }
+            if (collider != null)
+                collider.radius = radius;
+        }
+
+        private void Update()
+        {
+            //foreach (var coin in coinsDetector.CurrentObjects)
+            //{
+            //    Pull(coin);
+            //}
+        }
+
+        //private void Pull(Coin coin)
+        //{
+        //    var pullDirection = transform.position - coin.transform.position;
+        //    pullDirection.Normalize();
+        //    coin.SetPull(force * pullDirection);
+        //}
+    }
+}
--- Assets/James/InsertCoinGame/Ingame/Coins/Coin.cs
+++ Assets/James/InsertCoinGame/Ingame/Coins/Coin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TonhoHR.Utils;
 using UnityEngine;
 
 namespace James.InsertCoinGame.Ingame.Coins
@@ -11,9 +12,26 @@ namespace James.InsertCoinGame.Ingame.Coins
         private float KickForce;
         [SerializeField]
         private Rigidbody body;
+        private bool kicked;
+
         public void Kick(Vector3 direction)
         {
             body.AddForce(direction * KickForce, ForceMode.Impulse);
+            kicked = true;
+            this.WaitUntilThenDo(() => body.velocity.magnitude < .3f, KickOver);
+        }
+
+        private void KickOver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPull(Vector3 force)
+        {
+            if (kicked)
+                return;
+            body.velocity = force;
+            body.AddForce(force);
         }
     }
 }
